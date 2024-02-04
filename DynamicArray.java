01e838c2f69d0ae3c95c9e641cb862e4057c0f88
Asbjoernemil <asbjoernemil24@gmail.com>
2024-02-04 14:56:23 +0100

    shrink

--- DynamicArray.java
+++ DynamicArray.java
@@ -2,48 +2,19 @@ import java.util.Arrays;
 
 public class DynamicArray{
 private int size = 0;
-private Person[] array = new Person[10];
+private int growSize = 5;
+
+private static final int INITIAL_SIZE = 10;
+private Person[] array = new Person[INITIAL_SIZE];
 
 // .add
-public void add(Person person){
-    if(getSize() < array.length){
-
-        array[getSize()] = person;
-        setSize(getSize()+1);
-
-    } else {
-        System.out.println("Array full");
-    }
-    // System.out.println("getSize " + getSize());
-    // System.out.println("array length " + array.length);
-
-}
-
-// .remove
- public void remove() {
-        if (getSize() > 0) {
-            setSize(getSize() - 1);
-            printArray();
-        } else {
-            System.out.println("Array empty");
-        }
+public void add(Person person) {
+    if (getSize() == array.length) {
+        grow();
     }
 
-public void remove(int index) {
-    Person removePerson = array[index];
-    for (int i = 0; i < array.length; i++) {
-        if (array[i] == removePerson) {
-            if (index == array.length - 1) {
-                array[index] = null;
-            } else {
-                for (int j = i; j < array.length - 1; j++) {
-                    array[j] = array[j + 1];
-                }
-            }
-            setSize(getSize() - 1);
-        }
-    }
-    printArray();
+    array[getSize()] = person;
+    setSize(getSize() + 1);
 }
 
    public void printArray() {
@@ -58,6 +29,39 @@ public void remove(int index) {
     }
 }
 
+// .remove
+public void remove() {
+    if (getSize() > 0) {
+        setSize(getSize() - 1);
+        if (canShrink()) {
+            shrink();
+        }
+        printArray();
+    } else {
+        System.out.println("Array empty");
+    }
+}
+
+public void remove(int index) {
+    Person removePerson = array[index];
+    for (int i = 0; i < array.length; i++) {
+        if (array[i] == removePerson) {
+            if (index == array.length - 1) {
+                array[index] = null;
+            } else {
+                for (int j = i; j < array.length - 1; j++) {
+                    array[j] = array[j + 1];
+                }
+            }
+            setSize(getSize() - 1);
+            if (canShrink()) {
+                shrink();
+            }
+        }
+    }
+    printArray();
+}
+
 // Clear
 public void clear(){
     array = new Person[0];
@@ -66,9 +70,20 @@ public void clear(){
 }
 
 // Get
-public Person(int index){
+public Person get(int index){
     if(index >= 0 && index < getSize()){
         return array[index];
+    } else {
+        return null;
+    }
+}
+
+// Set
+public Person set(int index, Person person){
+    if (index >= 0 && index < getSize()) {
+        return array[index] = person;
+    } else {
+        return null;
     }
 }
 
@@ -80,6 +95,40 @@ public int getSize(){
 public int setSize(int size){
     this.size = size;
     return size;
+}
+
+// Grow
+private Person[] grow(){
+    int newSize = array.length * growSize;
+    Person[] newArray = new Person[newSize];
+
+    for(int i = 0; i < getSize(); i++){
+        newArray[i]= array[i];
+    }
+
+    array=newArray;
+    setSize(getSize());
+    return array;
+
+}
+
+// Shrink
+public boolean canShrink(){
+    int newSize = array.length - growSize;
+    return newSize >= INITIAL_SIZE;
+}
+
+public Person[] shrink(){
+    int newSize = array.length - growSize;
+    Person[] newArray = new Person[newSize];
+
+    for(int i = 0; i < newSize; i++){
+        newArray[i] = array[i];
+    }
+
+    array = newArray;
+        setSize(newSize);
+        return array;
 }
 
 
--- Main.java
+++ Main.java
@@ -17,20 +17,67 @@ public static void main(String[] args){
     darr.add(ron);
     darr.add(hermione);
     darr.add(asbjørn);
+    darr.add(asbjørn);
+    darr.add(asbjørn);
+    darr.add(asbjørn);
+    darr.add(asbjørn);
+    darr.add(asbjørn);
+    darr.add(asbjørn);
+    darr.add(asbjørn);
+    darr.add(asbjørn);
+    darr.add(asbjørn);
+    darr.add(asbjørn);
+    darr.add(asbjørn);
+    darr.add(asbjørn);
+    darr.add(asbjørn);
+    darr.add(asbjørn);
+    darr.add(asbjørn);
+    darr.add(asbjørn);
+    darr.add(asbjørn);
+    darr.add(asbjørn);
+    darr.add(asbjørn);
+
 
     // test .remove
     // Last
     darr.remove();
     // Index
     darr.remove(0);
+
+    // Shrink
+// Shrink
+System.out.println("Before shrink:");
+darr.printArray();
+
+System.out.println("Can shrink? " + darr.canShrink());
+darr.shrink();
+
+System.out.println("After shrink:");
+darr.printArray();
+
+    // Grow
+//  System.out.println("Before grow:");
+//         darr.printArray();
+//         darr.grow();
+//         System.out.println("After grow:");
+//         darr.printArray();
     
     // Clear
-    System.out.println("Before clear:");
-    darr.printArray();
+// System.out.println("Before clear:");
+// darr.printArray();
 
-    darr.clear();
+// darr.clear();
 
-    System.out.println("After clear:");
-    darr.printArray();
+// System.out.println("After clear:");
+// darr.printArray();
+
+    // Get
+Person retrievedPerson = darr.get(2);
+System.out.println("Retrieved person: " + retrievedPerson);
+
+    // Set
+Person newPerson = new Person("Mathias Hoff");
+darr.set(1, newPerson);
+darr.printArray();
 }
     }
